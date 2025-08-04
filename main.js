7b3aa29e0b97475530ad57929d970b2681cb0d38
jhoujhou <a01450887@gmail.com>
2025-08-04 21:42:49 +0800

    上傳作業

---
+++ main.js
@@ -0,0 +1,7 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
